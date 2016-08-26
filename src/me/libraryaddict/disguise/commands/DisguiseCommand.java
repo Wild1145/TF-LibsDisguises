a549c8b06b2597e518b70813beae6b9f9c6effc5
Lemon <minecraftoxlemonxo@gmail.com>
2016-08-26 20:52:14 +0500

    Unused import

--- src/me/libraryaddict/disguise/commands/DisguiseCommand.java
+++ src/me/libraryaddict/disguise/commands/DisguiseCommand.java
@@ -1,6 +1,5 @@
 package me.libraryaddict.disguise.commands;
 
-import com.sun.xml.internal.fastinfoset.util.CharArray;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
